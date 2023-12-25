41ed9902671709d5fdae9c0b3a8e8d1a18f6520c
Lauro Gripa <laurogripa@gmail.com>
2023-12-25 00:40:45 -0300

    Customize content

---
+++ src/components/YoutubeVideo.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const YouTubeVideo = ({ videoId, title }) => (
+  <iframe
+    width="560"
+    height="315"
+    src={`https://www.youtube.com/embed/${videoId}`}
+    title={title}
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>
+)
+
+export default YouTubeVideo
