3f999a35aea693cffe23812166a9d9c5944a4c2e
Lauro Gripa <laurogripa@gmail.com>
2024-07-13 20:21:23 -0300

    Adjust layout

--- components/about.tsx
+++ components/about.tsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="py-12 md:py-20 border-t border-gray-800">
+        <div className="py-12 md:py-20">
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <h1 className="h2 mb-4">Sobre o projeto</h1>
